21787a585e8ffab472d32a4b8c74568f5ce0eb2d
Deeksha M R <deekshamr37@gmail.com>
2025-06-11 23:53:23 +0530

    Multilingual Text Classifier

---
+++ model/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0bde33c8f8d31099df7b650f32b5951cc8029e8e1c5175e1c07fac5029f13aa1
+size 3129
---
+++ model/predict_reviews.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:02b6f78c8bff5ea2fe38bbf778a5bb615d3d1b2de8fd306778bf0d48fd374008
+size 11778
